dd9681fcc0077761581abf605796f0266232fc10
Tara Morrison <tm582@team101s-iMac.local>
2018-10-30 12:33:45 +0000

    added code to key script

--- Key_script.R
+++ Key_script.R
@@ -8,3 +8,30 @@ library(DESeq2)
 
 #Colours loaded for heatmaps
 hmcol = colorRampPalette(brewer.pal(9, 'GnBu'))(100)
+
+# DESeq from HTSeq
+ddsHTSeq=DESeqDataSetFromHTSeqCount(sampleTable = pdata, directory = '.', design= ~ condition)
+colData(ddsHTSeq)$condition=factor(colData(ddsHTSeq)$condition, levels=levels(pdata$condition))
+
+#Data normalisation
+dds=estimaddteSizeFactors(ddsHTSeq)
+dds=estimateDispersions(dds)
+
+normcounts <- counts(dds, normalized=TRUE)
+rawcounts=counts(dds,normalized=FALSE)
+log2counts=log2(normcounts+1)
+
+#VST
+vsd <- varianceStabilizingTransformation(dds)
+#from matrix
+vstMat <- assay(vsd)
+vstcounts <- vstMat[order(apply(vstMat,1,sum),decreasing =TRUE),]
+
+#Volcano Plots
+plot(RESULTSDATA$log2FoldChange,-log(RESULTSDATA$padj,10), ylab='-log10(Adjusted P)', xlab='Log2 FoldChange', main= 'Volcano Plot: Control vs CONDITION', pch=19, cex=0.2)
+text(RESULTSDATA[HITSDATA,]$log2FoldChange,-log(RESULTSDATA[HITSDATA,]$padj,10),labels=names[rownames(RESULTSDATA[HITSDATA,]),'COLUMN WITHIN DATA'],pos=3,cex=0.4)
+points(RESULTSDATA[HITSDATA,'log2FoldChange'],-log(RESULTSDATA[HITSDATA,'padj'],10),pch=21,cex=0.4,col='turquoise1')
+#Add horizotnal and vertical boudnary lines
+abline(v=-1, lty=3)
+abline(v=-1, lty=-3)
+abline(v=1, lty=3)
