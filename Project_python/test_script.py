cb48df52f6c46900f1132397e1c86aad37aa517c
tm582 <tm582@cam.ac.uk>
2019-01-28 15:14:13 +0000

    Python script test

---
+++ Project_python/test_script.py
@@ -0,0 +1,37 @@
+#cmd+S = save
+#ctrl+` = opens terminal
+#Shortcut to run script =ctrl+alt+n
+
+#Any functions are called using ()
+#All text in '' or ""
+#Triple quoates can be used to format long strings of text '''
+
+#Print fucntion is used to print messages
+print('hello')
+
+#Simple maths can be done in python
+print(2+2)
+print(1>3)
+
+#Able to set variables
+counts = 100
+print(counts)
+
+#Python uses 0 index thereofre the first character/number in an object is labelled 0 EXAMPLE below
+matrix_a = [2, 4, 6, 8, 10]
+#Length
+print(len(matrix_a))
+#First and last character respectively
+print(matrix_a[0])
+print(matrix_a[-1])
+
+#\ in python strings allow python to ignore the following character if it would affect the function
+#\n causes new line
+print('Python \'Programming')
+
+#joining expressions
+first = 'Tara'
+last = 'Morrison'
+full = f'{first} {last}'
+
+#
